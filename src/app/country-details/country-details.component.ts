aa5bc0f9f1c8d758af076ec6939709ed08e535a6
Krym <gumimash@gmail.com>
2024-03-16 16:02:48 +0200

    implemented fix for native name

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -11,12 +11,16 @@ export class CountryDetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   country: any;
   currency: any;
+  nativeName: any;
   constructor(private countryService: CountryService) {
     const countryName = this.route.snapshot.params['name'];
 
     this.countryService.getCountryByName(countryName).subscribe((data) => {
       this.country = data[0];
       this.currency = Object.keys(this.country.currencies)[0];
+      this.nativeName = (
+        Object.values(this.country.name.nativeName)[0] as any
+      ).common;
       console.log(this.country);
     });
   }
