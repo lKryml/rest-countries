3d45efe719d2cad32935da57b15d28c56822e459
Krym <gumimash@gmail.com>
2024-04-07 17:56:59 +0200

    implemented click on card instead of anchor

--- src/app/country-card/country-card.component.ts
+++ src/app/country-card/country-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CountryService } from '../country.service';
 import { Subscription, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-country-card',
@@ -9,7 +10,7 @@ import { Subscription, of } from 'rxjs';
 })
 export class CountryCardComponent implements OnInit, OnDestroy {
   private subscription?: Subscription;
-  constructor(private countryService: CountryService) {}
+  constructor(private countryService: CountryService, private router: Router) {}
   searchValue: string = '';
   regionValue: string = '';
   countries: any;
@@ -28,5 +29,8 @@ export class CountryCardComponent implements OnInit, OnDestroy {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
+  }
+  nav(name: string): void {
+    this.router.navigate(['/details', name]);
   }
 }
