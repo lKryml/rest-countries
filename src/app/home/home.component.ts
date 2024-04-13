029d5d9dd818c46080af1a5cb7e3cc3236ce8c5e
Krym <gumimash@gmail.com>
2024-04-13 23:50:29 +0200

    clean up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { CountryCardComponent } from '../country-card/country-card.component';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
